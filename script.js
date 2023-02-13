a7bb368c6c6f3f982ebf5a7abe30691a877319f2
Simon Conrad <simondbconrad@gmail.com>
2023-02-13 14:55:47 -0400

    fix slider functionality to output gridSize

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
 
+let gridSize = 16;
+
 output.innerHTML = `${slider.value} x ${slider.value}`;
 
 slider.oninput = function () {
-  output.innerHTML = `${this.value} x ${this.value}`;
+  output.innerHTML = `${this.value} x ${this.value}`; //rewrites grid-size display
+  gridSize = this.value; //sets grid-size variable
 };
 
-// const init = function {
-//     //read output from slide bar - store in variable for 'number of boxes'
-//     //create divs nested in container div
-// }
+const makeGrid = function () {};
+
+makeGrid();
