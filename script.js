09f83361a32963e9e00873bd04ee9111f173cafa
Simon Conrad <simondbconrad@gmail.com>
2023-02-14 15:19:10 -0400

    create makeGrid function to fill in css container with empty divs according to gridSize value from slider

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const slider = document.getElementById("myRange");
-const output = document.getElementById("demo");
+const sliderSize = document.getElementById("sizeRange");
+const output = document.getElementById("grid-size");
+let gridSize = sliderSize.value;
 
-let gridSize = 16;
+//INIT
+output.innerHTML = `${gridSize} x ${gridSize}`;
+console.log(gridSize);
 
-output.innerHTML = `${slider.value} x ${slider.value}`;
-
-slider.oninput = function () {
+//slider function - takes number for rows/columns and stores it in gridSize variable and displays it, returns gridSize
+sliderSize.oninput = function () {
   output.innerHTML = `${this.value} x ${this.value}`; //rewrites grid-size display
   gridSize = this.value; //sets grid-size variable
+  //   console.log(gridSize);
+  makeGrid(gridSize);
 };
 
-const makeGrid = function () {};
+//function to create grid takes in gridSize as a parameter, makes gridSize**2 div elements inside of the container
+const makeGrid = function (gridSize) {
+  let gridArea = gridSize ** 2;
+  for (let i = 0; i < gridArea; i++) {
+    let gridItem = document.createElement("div");
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    container.insertAdjacentElement("beforeend", gridItem);
+  }
+};
 
-makeGrid();
+// makeGrid(gridSize);
