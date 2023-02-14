ba2048d9d4ad57fe3df2fe191f1ff721c164070c
Simon Conrad <simondbconrad@gmail.com>
2023-02-14 15:42:10 -0400

    clean up code format

--- script.js
+++ script.js
@@ -2,18 +2,6 @@ const sliderSize = document.getElementById("sizeRange");
 const output = document.getElementById("grid-size");
 let gridSize = sliderSize.value;
 
-//INIT
-output.innerHTML = `${gridSize} x ${gridSize}`;
-console.log(gridSize);
-
-//slider function - takes number for rows/columns and stores it in gridSize variable and displays it, returns gridSize
-sliderSize.oninput = function () {
-  output.innerHTML = `${this.value} x ${this.value}`; //rewrites grid-size display
-  gridSize = this.value; //sets grid-size variable
-  //   console.log(gridSize);
-  makeGrid(gridSize);
-};
-
 //function to create grid takes in gridSize as a parameter, makes gridSize**2 div elements inside of the container
 const makeGrid = function (gridSize) {
   let gridArea = gridSize ** 2;
@@ -25,4 +13,14 @@ const makeGrid = function (gridSize) {
   }
 };
 
-// makeGrid(gridSize);
+//slider function - takes number for rows/columns and stores it in gridSize variable and displays it, returns gridSize
+sliderSize.oninput = function () {
+  output.innerHTML = `${this.value} x ${this.value}`; //rewrites grid-size display
+  gridSize = this.value; //sets grid-size variable
+  //   console.log(gridSize);
+  makeGrid(gridSize);
+};
+
+//INIT
+output.innerHTML = `${gridSize} x ${gridSize}`;
+makeGrid(gridSize);
