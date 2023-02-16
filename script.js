82ef82c8f8c4af7bafac17c74e5e99141a53657e
Simon Conrad <simondbconrad@gmail.com>
2023-02-16 12:06:16 -0400

    create changeSize function

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const makeGrid = function (size) {
   let board = document.querySelector(".container");
+  let cells = board.querySelectorAll("div");
+  cells.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < 256; i++) {
+  let gridCells = size * size;
+  for (let i = 0; i < gridCells; i++) {
     let cell = document.createElement("div");
     cell.style.backgroundColor = "rgb(243, 92, 200)";
     board.insertAdjacentElement("beforeend", cell);
@@ -11,3 +14,11 @@ const makeGrid = function (size) {
 };
 
 makeGrid(16);
+
+const changeSize = function (input) {
+  if (input >= 16 && input <= 100) {
+    makeGrid(input);
+  } else {
+    console.log("broh number between 16 and 100 broh 🐳");
+  }
+};
